242eac9106117d00a2ad988b30b7b03c215a6331
Blaksmith <blak@uhtc.net>
2018-01-06 14:17:52 -0500

    Added logging for when it stops due to balance.

--- Blaks-Bouncing-Fibber.lua
+++ Blaks-Bouncing-Fibber.lua
@@ -3,13 +3,14 @@
 
 minbet = 0.00000001 -- Set this according to the coin / token you are on!
 autotune = false -- Set to false to use static settings below
+smoothRecover = false -- set this to false to just stop betting when nextbet is greater than your balance
 
 restTime = 0.0 -- How long to wait in seconds before the next bet.  Some sites need this
 			   -- Bitvest setting 0.7 for low bet values
  
 basebet = 0.00000001
 basechance=7 -- The chance that you would like use.  7 seems to be a good starting point
-housePercent = 5.0 -- Set this according to the site you are on.
+housePercent = 1.5 -- Set this according to the site you are on.
 -- Known site percentages
 -- Freebitco.in = 5%
 -- Bitsler = 1.5%
@@ -19,6 +20,7 @@ fibstep = 0.0925 -- Fibonacci stepping amount
 chanceStep = 0.01 -- Chance stepping amount 
 
 enableLogging = true -- Set to false for no logging
+apppendlog = true -- This must be set to false for the very first run!
 filename = "bouncer.csv" -- Default to the directory where dicebot is run from.
 tempfile = "tempfile.log" -- You can add an absolute directory if wanted with: C:\directory etc
 rollLog = 50 -- Use 0 for dynamic long streak logging, otherwise put in a value to log after X losing streak
@@ -42,7 +44,7 @@ rollCount = 0
 rollSeedCount = 0
 profit = 0
 
-if(enableLogging == true) then
+if(appendlog != true) then
 	fin = assert(io.open(filename, "w"))
 	fin:write("Timestamp, Bet ID, Streak, Bet, Chance, fibstep, Spent, Win Amount, Win Profit\n")
 	fin:close()
@@ -156,7 +158,57 @@ function dobet()
 			nextbet = myfib(stepCount)  
 		end
 		if nextbet >= balance then -- Keep rolling, without completely busting.  May add flag to disable
-			nextbet = balance / 2 -- Don't completely bust, but try and recover something
+			if(smoothRecover == true) then
+				nextbet = balance / 2 -- Don't completely bust, but try and recover something
+			else
+				print("Balance too low for the bet.  Stopping")
+
+				if(enableLogging == true) then
+					tempstr = "year-0month-0day 0hour:0minute:0second, betid, streak, bet, chance, fibstep, spent, winamount, profit *******LOSING STOP*******\n"
+					tempstr = string.gsub(tempstr, "year", lastBet.date.year)
+					if (lastBet.date.month >= 10) then tempstr = string.gsub(tempstr, "0month", "month") end 	
+					if (lastBet.date.day >= 10) then tempstr = string.gsub(tempstr, "0day", "day") end 	
+					if (lastBet.date.hour >= 10) then tempstr = string.gsub(tempstr, "0hour", "hour") end 	
+					if (lastBet.date.minute >= 10) then tempstr = string.gsub(tempstr, "0minute", "minute") end 	
+					if (lastBet.date.second >= 10) then tempstr = string.gsub(tempstr, "0second", "second") end 	
+					tempstr = string.gsub(tempstr, "month", lastBet.date.month)			
+					tempstr = string.gsub(tempstr, "day", lastBet.date.day)			
+					tempstr = string.gsub(tempstr, "hour", lastBet.date.hour)			
+					tempstr = string.gsub(tempstr, "minute", lastBet.date.minute)			
+					tempstr = string.gsub(tempstr, "second", lastBet.date.second)
+					tempstr = string.gsub(tempstr, "betid", lastBet.Id)
+					tempstr = string.gsub(tempstr, "streak", lossCount)
+					tempcalc = string.format("%.8f", nextbet)
+					tempstr = string.gsub(tempstr, "bet", tempcalc)
+					tempcalc = string.format("%.2f", chance)
+					tempstr = string.gsub(tempstr, "chance", tempcalc)
+					tempstr = string.gsub(tempstr, "fibstep", fibstep)
+					tempcalc = string.format("%.8f", spent)
+					tempstr = string.gsub(tempstr, "spent", tempcalc)
+					tempcalc = string.format("%.8f", lastBet.Profit)
+					tempstr = string.gsub(tempstr, "winamount", tempcalc)
+					profit = lastBet.Profit - spent
+					tempcalc = string.format("%.8f", profit)
+					tempstr = string.gsub(tempstr, "profit", tempcalc)
+					-- print(tempstr)
+		
+					fin = assert(io.open(filename, "r"))
+					content = fin:read("*a")
+					fin:close()
+					
+					fout = assert(io.open(tempfile, "w"))
+					fout:write(content)
+					fout:write(tempstr)
+					-- fout:write(oppositeOut)
+					
+					fout:close()
+					os.remove(filename) 
+					os.rename(tempfile, filename)
+		
+				end
+
+				stop()
+			end
 		end
 	end
 
